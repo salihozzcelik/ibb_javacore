9446246623165a5daa2aca28ec18ef12b1dea957
salih <salihozzcelik@gmail.com>
2025-02-12 00:59:47 +0300

    operatörler

---
+++ src/main/java/com/salihozcelik/_1_part_javatypes/_04_Operators.java
@@ -0,0 +1,22 @@
+package com.salihozcelik._1_part_javatypes;
+
+public class _04_Operators {
+    public static void main(String[] args) {
+        int number1 = 5, number2 = 4;
+        int addition = number1 + number2;
+        int subtraction = number1 - number2;
+        int multiplication = number1 * number2;
+        int division = number1 / number2;
+        int modulus = number1 % number2;
+
+        System.out.println("toplam " + addition);
+        System.out.println("çıkarma " + subtraction);
+        System.out.println("çarpma " + multiplication);
+        System.out.println("bölme " + division);
+        System.out.println("kalan " + modulus);
+
+        // Mantıksal Operators: &&=VE ||=VEYA !=değil
+        // ++ 1 artır,  -- 1 azalt
+
+    }
+}
