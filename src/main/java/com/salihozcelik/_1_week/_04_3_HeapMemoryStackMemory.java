097b309afae8931b2d1c9363973839bd1bf9219f
salih <salihozzcelik@gmail.com>
2025-02-14 02:03:36 +0300

    örnekler

---
+++ src/main/java/com/salihozcelik/_1_week/_04_3_HeapMemoryStackMemory.java
@@ -0,0 +1,9 @@
+package com.salihozcelik._1_week;
+
+public class _04_3_HeapMemoryStackMemory {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/com/salihozcelik/_1_week/_04_4_Cast.java
@@ -0,0 +1,9 @@
+package com.salihozcelik._1_week;
+
+public class _04_4_Cast {
+
+    public static void main(String[] args) {
+
+
+    }
+}
