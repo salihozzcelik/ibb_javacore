2a614ce5eb0394b1e7432a5244c35d3907312b36
salih <salihozzcelik@gmail.com>
2025-02-16 01:29:17 +0300

    örnekler

---
+++ src/main/java/com/salihozcelik/_2_week/_9_2_Conditional.java
@@ -0,0 +1,12 @@
+package com.salihozcelik._2_week;
+
+public class _9_2_Conditional {
+    public static void main(String[] args) {
+        int number=5;
+        if (number>0)
+            System.out.println("pozitif");
+        else
+            System.out.println("negatif");
+
+    }
+}
---
+++ src/main/java/com/salihozcelik/_2_week/_9_2_Conditional_ternary.java
@@ -0,0 +1,9 @@
+package com.salihozcelik._2_week;
+
+public class _9_2_Conditional_ternary {
+    public static void main(String[] args) {
+        int number = 5;
+        String result = (number > 0) ? "pozitif" : "negatif";
+        System.out.println(result);
+    }
+}
